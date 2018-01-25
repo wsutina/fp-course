781bafd04d58ff08f0b5c3e457f947c1fd9a098a
Sutina Wipawiwat <wsutina@mgail.com>
2018-01-25 14:40:09 +1100

    Implement mapOptional

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -27,8 +27,8 @@ mapOptional ::
   (a -> b)
   -> Optional a
   -> Optional b
-mapOptional =
-  error "todo: Course.Optional#mapOptional"
+mapOptional f (Full a) = Full (f a)
+mapOptional _ Empty = Empty
 
 -- | Bind the given function on the possible value.
 --
